3afdeb57da868ce6b7ff04742a10667deed9a5c9
Arnesh Regmi <arneshregmi@Arneshs-MacBook-Pro.local>
2019-01-05 18:16:34 -0800

    Train Scheduler working with firebase database

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -39,16 +39,67 @@ function addTrainToFirebase(){
 
         trainNames.push(trainName);
 
+        firstTrainTime = firstTrainTime.split(":");
+        console.log("firsttraintime"+firstTrainTime);
+        firstTrainTimeInMinutes = parseInt(firstTrainTime[0])*60+parseInt(firstTrainTime[1]);
+        console.log("firstfTrainTimeInMinutes"+firstTrainTimeInMinutes);
+
+ 
+        var currtime = moment().format('HH:mm:ss');
+        console.log("NOW"+currtime)
+        currtime = currtime.split(":")
+        currtimeInMinutes = parseInt(currtime[0])*60+parseInt(currtime[1]);
+        var nextTrain = firstTrainTimeInMinutes;
+
+        console.log("CurrTime"+currtimeInMinutes)
+        console.log("nexttrain"+nextTrain);
+
+        while(nextTrain < currtimeInMinutes)
+        {
+            console.log(nextTrain)
+            nextTrain = nextTrain+parseInt(frequency);
+        }
+    
+        
+        console.log("Next Train"+nextTrain)
+        
+        nextTrainTime = Math.floor(nextTrain / 60) + ':' + nextTrain % 60
+
+        nextTrainFromNow = nextTrain - currtimeInMinutes;
+        nextTrainFromNowHM =  Math.floor(nextTrainFromNow/ 60) + ':' + nextTrainFromNow % 60
+       
+
         database.ref(trainName).set({
         Destination: destination,
         Frequency: frequency,
-        NextArrival : "01:39 PM",
-        MinutesAway: "1154"
+        NextArrival : nextTrainTime,
+        zMinutesAway: nextTrainFromNowHM
       });
 
         var body = $("body");
         var table = body.find("#table");
         var tablestring = "<tr><td>"+ trainName 
+
+        /*
+
+        first train time: 5:15 mins
+
+
+        frequency: 20 mins
+
+        5:35, 5:55, 6:15, 
+
+        next train time: ?
+
+        In how many minutes: ?
+
+
+
+
+
+
+
+        */
 
 
       
